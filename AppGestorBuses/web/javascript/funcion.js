68298879c3fedc9f36816db1061c2a5f101f1c90
ivlarks <marcosdmayen@gmail.com>
2015-09-20 15:33:05 -0600

    ctm

--- AppGestorBuses/web/javascript/funcion.js
+++
@@ -1,8 +0,0 @@
-function cargarArchivo(elemento){
-    var file = elemento.files[0];
-    var obHidden = document.formulario.nombre;
-    obHidden.value = file.name;
-    document.formulario.target ="null";
-    document.formulario.action = "CargaCSV";
-    document.formulario.submit();
-}
